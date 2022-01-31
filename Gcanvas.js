d45fba6470fedd9f3854f075e0827aa49576ead6
Mathias Mantai <mantai@meuser.it>
2022-01-31 16:56:23 +0100

    canvas blur tmp fix

--- Gcanvas.js
+++ Gcanvas.js
@@ -188,5 +188,28 @@ class Gcanvas {
     infoLog(infoMessage) {
         console.log(infoMessage);
     }
+
+    /**
+     * Crisp canvas
+     */
+    crispCanvas() {
+        //get DPI
+let dpi = window.devicePixelRatio;
+//get canvas
+let canvas = document.getElementById('myCanvas');
+//get context
+let ctx = canvas.getContext('2d');
+function fix_dpi() {
+//get CSS height
+//the + prefix casts it to an integer
+//the slice method gets rid of "px"
+let style_height = +getComputedStyle(canvas).getPropertyValue("height").slice(0, -2);
+//get CSS width
+let style_width = +getComputedStyle(canvas).getPropertyValue("width").slice(0, -2);
+//scale the canvas
+canvas.setAttribute('height', style_height * dpi);
+canvas.setAttribute('width', style_width * dpi);
+    }
      
+}
 }
